4e6e97893eba576a3093547f9f69f54efa76aec5
yucc <e-Chuancheng.Yu@geely.com>
2023-06-15 17:33:12 +0800

    1.优化AI绘图板块结构布局及代码 2.提取输入框组件作为插槽

---
+++ src/components/common/SubmitFooter/submitFooter.vue
@@ -0,0 +1,87 @@
+<template>
+	<footer :class="footerClass">
+		<div class="w-full max-w-screen-xl m-auto">
+			<div class="flex items-center justify-between space-x-2">
+				<slot></slot>
+				<NAutoComplete :options="props.searchOptions" :render-label="props.renderOption">
+					<template #default="{ handleInput, handleBlur, handleFocus }">
+						<NInput
+							ref="inputRef"
+							v-model:value="autoValue"
+							type="textarea"
+							:placeholder="props.placeholder"
+							:autosize="{ minRows: 1, maxRows: isMobile ? 4 : 8 }"
+							@input="(value)=>{ emits('update:modelValue',value);handleInput(value)} "
+							@focus="handleFocus"
+							@blur="handleBlur"
+							@keypress="handleEnter"
+						/>
+					</template>
+				</NAutoComplete>
+				<NButton type="primary" :disabled="props.buttonDisabled" @click="emits('submit', autoValue.value)">
+					<template #icon>
+              <span class="dark:text-black">
+                <SvgIcon :icon="props.svgIcon"/>
+              </span>
+					</template>
+				</NButton>
+			</div>
+		</div>
+	</footer>
+
+
+</template>
+
+<script lang="ts" setup>
+import {computed, defineOptions, ref, watch} from "vue";
+import {useBasicLayout} from "@/hooks/useBasicLayout";
+import {NButton, NAutoComplete, NInput} from 'naive-ui'
+import type {AutoCompleteOption, AutoCompleteGroupOption} from 'naive-ui'
+import {SvgIcon} from '@/components/common'
+
+defineOptions({name: 'submitFooter'})
+const {isMobile} = useBasicLayout()
+const footerClass = computed(() => {
+	if (isMobile.value) return ['sticky', 'left-0', 'bottom-0', 'right-0', 'p-2', 'pr-3', 'overflow-hidden']
+	return ['p-4']
+})
+
+const props = withDefaults(defineProps<{
+	placeholder: string,
+	searchOptions: Array<string | AutoCompleteOption | AutoCompleteGroupOption>,
+	svgIcon: string,
+	modelValue: string
+	renderOption: any //参考naive-ui (info: { node: VNode, option: SelectOption | SelectGroupOption, selected: boolean }) => VNodeChild
+	buttonDisabled: boolean
+}>(), {
+	placeholder: '请输入...',
+	svgIcon: 'ri:send-plane-fill',
+	modelValue: ''
+})
+const autoValue = ref<string>(props.modelValue)
+watch(() => props.modelValue, () => autoValue.value = props.modelValue)
+const emits = defineEmits<{
+	(event: 'submit', value: string): void
+	(event: 'update:modelValue', value: string): void
+}>()
+
+
+const handleEnter = (event: KeyboardEvent) => {
+	if (!isMobile.value) {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			event.preventDefault()
+			emits('submit', autoValue.value)
+		}
+	} else {
+		if (event.key === 'Enter' && event.ctrlKey) {
+			event.preventDefault()
+			emits('submit', autoValue.value)
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/chat/layout/image/index.vue
+++ src/views/chat/layout/image/index.vue
@@ -1,65 +1,34 @@
 <template>
 	<div style="">
+		<NRow>
+			<NCol span="12">
+				<NStatistic label="已使用次数" :value="usedCount">
+					<template #suffix>
+						/ 100
+					</template>
+				</NStatistic>
+			</NCol>
+			<NCol :span="12">
+				<NStatistic label="当前队列等待任务数" :value="waitingCount"></NStatistic>
+			</NCol>
+		</NRow>
 
-			<NRow>
-				<NCol span="12">
-					<NStatistic label="已使用次数" :value="usedCount">
-						<template #suffix>
-							/ 100
-						</template>
-					</NStatistic>
-				</NCol>
-				<NCol :span="12">
-					<NStatistic label="当前队列等待任务数" :value="waitingCount"></NStatistic>
-				</NCol>
-			</NRow>
-
-		<div>
-			<h1 style="font-size: 24px;">图片验证码：</h1>
-			<NInput
-				placeholder="请输入图片验证码"
-				v-model:value="verifycationCode"
-			/>
-		</div>
-		<div class="flex flex-wrap items-center gap-4">
-			<label>模型选择：</label>
-			<NSelect
-				style="width: 140px"
-				v-model:value="modeltype"
-				:options="modelOptions"
-			/>
-		</div>
-		<h1 style="font-size: 24px;">图片描述词：</h1>
-		<div class="flex items-center justify-between space-x-2">
-			<NAutoComplete>
-				<NInput
-					placeholder="请输入图片描述词,回车可换行"
-					ref="inputRef"
-					type="textarea"
-					:autosize="{ minRows: 4, maxRows: 10 }"
-					v-model:value="description"
-				/>
-			</NAutoComplete>
-			<NButton type="primary" @click="submit">
-				生成
-			</NButton>
-		</div>
-		<div v-show="modeltype !== 1">
+		<div v-show="formData.modelType !== 1">
 			<span style="font-size: 24px;">返回图片数量1-10，本站不保存图片，需要请立即下载。</span>
 			<NSpace vertical>
-				<NSlider v-model:value="numOfImages" :max="10" :min="1"/>
+				<NSlider v-model:value="formData.Count" :max="10" :min="1"/>
 				<NInputNumber v-model:value="numOfImages" size="small" :max="10" :min="1"/>
 			</NSpace>
 		</div>
 		<div
 			id="image-scroll-container"
 			style="
-      overflow: auto;
-      height: 1000px;
-      display: flex;
-      flex-direction: column;
-      gap: 8px;
-    "
+		      overflow: auto;
+		      height: 450px;
+		      display: flex;
+		      flex-direction: column;
+		      gap: 8px;
+		    "
 		>
 			<NImage
 				v-for="(imageUrl, index) in imgUrl"
@@ -69,124 +38,138 @@
 				lazy
 				:src="imageUrl"
 				:intersection-observer-options="{
-        root: '#image-scroll-container',
-      }"
+		        root: '#image-scroll-container',
+		      }"
 				style="margin-bottom: 10px;"
 			>
 				<template #placeholder>
 					<div
 						style="
-            width: 100px;
-            height: 100px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #0001;
-          "
+		            width: 100px;
+		            height: 100px;
+		            display: flex;
+		            align-items: center;
+		            justify-content: center;
+		            background-color: #0001;
+		          "
 					>
 						Loading
 					</div>
 				</template>
 			</NImage>
 		</div>
+		<submit-footer v-model="formData.Prompt" placeholder="请输入图片描述词" @submit="submit">
+			<SvgIcon icon="ri:settings-4-line" @click="showModal=true" style="font-size: 1.5rem;cursor: pointer"/>
+		</submit-footer>
+
+		<NModal v-model:show="showModal" style="width: 90%; max-width: 600px;" preset="card">
+			<NForm model="formData" :rules="formRules"
+						 ref="formRef"
+						 label-placement="left"
+						 label-width="auto"
+						 require-mark-placement="right-hanging">
+				<NFormItem label="图片验证码" path="verifycationCode">
+					<NInput v-model="formData.verifycationCode"></NInput>
+				</NFormItem>
+				<NFormItem label="模型" path="modelType">
+					<NSelect
+						style="width: 140px"
+						v-model:value="formData.modelType"
+						:options="modelOptions"
+					/>
+
+				</NFormItem>
+
+			</NForm>
+
+		</NModal>
+
 	</div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import {
 	NImage,
 	NSelect,
-	NAutoComplete,
 	NInput,
 	NButton,
 	NSpace,
 	NSlider,
 	NInputNumber,
 	useMessage,
-	NCol, NStatistic, NIcon, NRow
-
+	NCol,
+	NStatistic,
+	NIcon,
+	NRow,
+	NModal,
+	NForm,
+	NFormItem
 } from "naive-ui";
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 import axios from 'axios';
 import {useAuthStoreWithout} from '@/store/modules/auth'
 import {useSignalR} from '@/views/chat/hooks/useSignalR';
+import SubmitFooter from "@/components/common/SubmitFooter/submitFooter.vue";
+import {SvgIcon} from '@/components/common'
 // 定义后端接口的地址
 const apiUrl = import.meta.env.VITE_GLOB_API_URL;
 const apiBaseUrl = import.meta.env.VITE_APP_API_BASE_URL;
+const authStore = useAuthStoreWithout();
+const numOfImages = ref(1); // 初始值为1
+const usedCount = ref(0);//已生成图片数量
+const modelOptions: Array<{ label: string; value: number }> = [
+	{label: 'CHATGPT', value: 0},
+	{label: 'SD', value: 1},
+];
+const showModal = ref(false)
 
-export default {
-	components: {
-		NImage,
-		NAutoComplete,
-		NInput,
-		NButton,
-		NSpace,
-		NSlider,
-		NInputNumber,
-		NSelect,
-		NCol,
-		NStatistic,
-		NIcon,
-		NRow
-	},
-	setup() {
-		const authStore = useAuthStoreWithout();
-		const numOfImages = ref(1); // 初始值为1
-		const description = ref("");
-		const verifycationCode = ref(authStore.imgKey ?? "");
-		const modeltype = ref(1);
-		const usedCount = ref(0);//已生成图片数量
-		const modelOptions: { label: string; value: number }[] = [
-			{label: 'CHATGPT', value: 0},
-			{label: 'SD', value: 1},
-		];
-		const ms = useMessage();
+const onPositiveClick = () => {
+}
+const formRules = {}
+const ms = useMessage();
+//signalR
+const {waitingCount, connection, imgUrl} = useSignalR(apiBaseUrl + '/graphhub');
+
+type SubmitDTO = {
+	verifycationCode: string
+	Prompt: string
+	modelType: number
+	connectionId: any
+	Count: number
+	SizeType: number
+}
+
+const formData = reactive<SubmitDTO>({
+	Prompt: "",
+	Count: 1,
+	SizeType: 1,
+	verifycationCode: authStore.imgKey ?? "",
+	modelType: 1,
+	connectionId: connection.value?.connectionId
+})
+
+
+const submit = async () => {
+	if (!connection.value) {
+		ms.warning('页面已失效，请刷新页面！');
+		return;
+	}
+
+	try {
+		authStore.setImgKey(formData.verifycationCode);
 		//signalR
-		const {waitingCount, connection, imgUrl} = useSignalR(apiBaseUrl + '/graphhub');
+		const response = await axios.post(apiUrl + '/GenerateGraph', formData);
+		if (response.data.status === 'Fail') {
+			ms.error(response.data.message ?? 'error')
+			return
+		}
+		usedCount.value = response.data.data;
+		ms.success(response.data.message);
 
-		const submit = async () => {
-			if (!connection.value) {
-				ms.warning('页面已失效，请刷新页面！');
-				return;
-			}
-			const params = {
-				Prompt: description.value,
-				Count: numOfImages.value,
-				SizeType: 1,
-				verifycationCode: verifycationCode.value,
-				modelType: modeltype.value,
-				connectionId: connection.value?.connectionId
-			};
-			try {
-				authStore.setImgKey(verifycationCode.value);
-				//signalR
-				const response = await axios.post(apiUrl + '/GenerateGraph', params);
-				if (response.data.status === 'Fail') {
-					ms.error(response.data.message ?? 'error')
-					return
-				}
-				usedCount.value = response.data.data;
-				ms.success(response.data.message);
-
-			} catch (error) {
-				console.log(`请求失败：${error}`);
-				ms.error('报错拉！：' + error);
-			}
-			console.log("提交的参数：", params); // 在控制台输出提交的参数
-		};
-
-		return {
-			numOfImages,
-			description,
-			verifycationCode,
-			modeltype,
-			modelOptions,
-			//signal
-			waitingCount,
-			imgUrl,
-			submit,
-			usedCount
-		};
-	},
+	} catch (error) {
+		console.log(`请求失败：${error}`);
+		ms.error('报错拉！：' + error);
+	}
+	console.log("提交的参数：", formData); // 在控制台输出提交的参数
 };
 </script>
