1dcdbcb59530297de7b7df9b1b7e950aa32553d8
= <=>
2023-07-08 21:00:51 +0800

    1.登录页响应式 2.chatGpt 输入框字数限制展示

--- src/components/common/SubmitFooter/submitFooter.vue
+++ src/components/common/SubmitFooter/submitFooter.vue
@@ -1,71 +1,38 @@
-<template>
-	<footer :class="footerClass">
-		<div class="w-full max-w-screen-xl m-auto">
-			<div class="flex items-center justify-between space-x-2">
-				<slot></slot>
-				<NAutoComplete :options="props.searchOptions" :render-label="props.renderOption">
-					<template #default="{ handleInput, handleBlur, handleFocus }">
-						<NInput
-							ref="inputRef"
-							v-model:value="autoValue"
-							type="textarea"
-							:placeholder="props.placeholder"
-							:autosize="{ minRows: 1, maxRows: isMobile ? 4 : 8 }"
-							@input="(value)=>{ emits('update:modelValue',value);handleInput(value)} "
-							@focus="handleFocus"
-							@blur="handleBlur"
-							@keypress="handleEnter"
-						/>
-					</template>
-				</NAutoComplete>
-				<NButton type="primary" :disabled="props.buttonDisabled" @click="emits('submit', autoValue)">
-					<template #icon>
-              <span class="dark:text-black">
-                <SvgIcon :icon="props.svgIcon"/>
-              </span>
-					</template>
-				</NButton>
-			</div>
-		</div>
-	</footer>
-
-
-</template>
-
 <script lang="ts" setup>
-import {computed, defineOptions as defineOptionsFromVue, ref, watch} from "vue";
-import {useBasicLayout} from "@/hooks/useBasicLayout";
-import {NButton, NAutoComplete, NInput} from 'naive-ui'
-import type {AutoCompleteOption, AutoCompleteGroupOption} from 'naive-ui'
+import {computed, defineOptions as defineOptionsFromVue, ref, watch} from 'vue'
+import {NAutoComplete, NButton, NInput} from 'naive-ui'
+import type {AutoCompleteGroupOption, AutoCompleteOption} from 'naive-ui'
+import {useBasicLayout} from '@/hooks/useBasicLayout'
 import {SvgIcon} from '@/components/common'
 
-defineOptionsFromVue({name: 'submitFooter'})
-const {isMobile} = useBasicLayout()
-const footerClass = computed(() => {
-	if (isMobile.value) return ['sticky', 'left-0', 'bottom-0', 'right-0', 'p-2', 'pr-3', 'overflow-hidden']
-	return ['p-4']
-})
-
 const props = withDefaults(defineProps<{
-	placeholder: string,
-	searchOptions: Array<string | AutoCompleteOption | AutoCompleteGroupOption>,
-	svgIcon: string,
-	modelValue: string,
-	renderOption: any //参考naive-ui (info: { node: VNode, option: SelectOption | SelectGroupOption, selected: boolean }) => VNodeChild
+	placeholder: string
+	searchOptions: Array<string | AutoCompleteOption | AutoCompleteGroupOption>
+	svgIcon: string
+	modelValue: string
+	renderOption: any // 参考naive-ui (info: { node: VNode, option: SelectOption | SelectGroupOption, selected: boolean }) => VNodeChild
 	buttonDisabled: boolean
+	counter: number
 }>(), {
 	placeholder: '请输入...',
 	svgIcon: 'ri:send-plane-fill',
-	modelValue: ''
+	modelValue: '',
 })
-const autoValue = ref<string>(props.modelValue)
-watch(() => props.modelValue, () => autoValue.value = props.modelValue)
 const emits = defineEmits<{
 	(event: 'submit', value: string): void
 	(event: 'update:modelValue', value: string): void
 }>()
+defineOptionsFromVue({name: 'submitFooter'})
+const {isMobile} = useBasicLayout()
+const footerClass = computed(() => {
+	if (isMobile.value) {
+		return ['sticky', 'left-0', 'bottom-0', 'right-0', 'p-2', 'pr-3', 'overflow-hidden']
+	}
+	return ['p-4']
+})
 
-
+const autoValue = ref<string>(props.modelValue)
+watch(() => props.modelValue, () => autoValue.value = props.modelValue)
 const handleEnter = (event: KeyboardEvent) => {
 	if (!isMobile.value) {
 		if (event.key === 'Enter' && !event.shiftKey) {
@@ -79,8 +46,41 @@ const handleEnter = (event: KeyboardEvent) => {
 		}
 	}
 }
-
 </script>
+
+<template>
+	<footer :class="footerClass">
+		<div class="w-full max-w-screen-xl m-auto">
+			<div class="flex items-center justify-between space-x-2">
+				<slot/>
+				<NAutoComplete :options="props.searchOptions" :render-label="props.renderOption">
+					<template #default="{ handleInput, handleBlur, handleFocus }">
+						<NInput
+							ref="inputRef"
+							v-model:value="autoValue"
+							type="textarea"
+							:placeholder="props.placeholder"
+							:autosize="{ minRows: 1, maxRows: isMobile ? 4 : 8 }"
+							@input="(value) => { emits('update:modelValue', value);handleInput(value) } "
+							@focus="handleFocus"
+							@blur="handleBlur"
+							@keypress="handleEnter"
+							:maxlength="counter"
+							show-count
+						/>
+					</template>
+				</NAutoComplete>
+				<NButton type="primary" :disabled="props.buttonDisabled" @click="emits('submit', autoValue)">
+					<template #icon>
+            <span class="dark:text-black">
+              <SvgIcon :icon="props.svgIcon"/>
+            </span>
+					</template>
+				</NButton>
+			</div>
+		</div>
+	</footer>
+</template>
 
 <style scoped>
 
--- src/views/chat/layout/Login.vue
+++ src/views/chat/layout/Login.vue
@@ -1,116 +1,149 @@
-<template>
-	<div class="h-full relative bg-center bg-cover bg-no-repeat" :style="{backgroundImage:`url(${backgroundImageURL})`}">
-		<div class="absolute w-1/4 p-4 right-40 bottom-1/2 translate-y-1/2">
-			<NSpin :show="loading">
-				<h2 class="text-white text-center pb-4 text-2xl font-mono font-bold">TERRA MOURS</h2>
-				<NForm ref="formRef" size="medium" label-placement="left" :model="formData" :rules="formRules">
-					<NFormItem path="userAccount">
-						<NInput placeholder="请输入用户名" v-model:value="formData.userAccount">
-							<template #prefix>
-								<SvgIcon icon="ant-design:user-outlined"/>
-							</template>
-						</NInput>
-					</NFormItem>
-					<NFormItem path="userPassword">
-						<NInput type="password" show-password-on="click" placeholder="请输入密码" v-model:value="formData.userPassword">
-							<template #prefix>
-								<SvgIcon icon="ant-design:unlock-outlined"/>
-							</template>
-						</NInput>
-					</NFormItem>
-					<NSpace :vertical="true" :size="24">
-						<div class="flex-y-center justify-between">
-							<NCheckbox>记住我</NCheckbox>
-							<NButton :text="true">忘记密码？</NButton>
-						</div>
-						<NButton
-							type="primary"
-							size="large"
-							:block="true"
-							@click="loginHandle"
-						>
-							确定
-						</NButton>
-						<div class="flex-y-center justify-between">
-							<!-- 手机验收码登录 -->
-							<!-- <NButton class="flex-1" :block="true" @click="toLoginModule('code-login')">
-								{{ loginModuleLabels['code-login'] }}
-							</NButton> -->
-
-							<div class="w-12px"></div>
-							<NButton class="flex-1" :block="true" @click="toRegister">
-								注册
-							</NButton>
-						</div>
-					</NSpace>
-					<!-- 其他账户登录 -->
-					<!-- <other-account @login="handleLoginOtherAccount" /> -->
-				</NForm>
-			</NSpin>
-		</div>
-
-	</div>
-</template>
-
 <script setup lang="ts">
-import {reactive, ref} from 'vue';
-import {NForm, NSpin, NFormItem, NInput, NSpace, NCheckbox, NButton, FormInst} from 'naive-ui';
+import { reactive, ref } from 'vue'
+import type { FormInst } from 'naive-ui'
+import { NButton, NCheckbox, NForm, NFormItem, NGrid, NGridItem, NInput, NSpace, NSpin, useMessage } from 'naive-ui'
+import { useRouter } from 'vue-router'
 import SvgIcon from '@/components/common/SvgIcon/index.vue'
-import {login} from "@/api";
-import {useAuthStore} from '@/store/modules/auth/index'
-import {useMessage} from 'naive-ui'
-import {useRouter} from 'vue-router'
+import { login } from '@/api'
+import { useAuthStore } from '@/store'
+
+defineProps<Props>()
 
 // import { OtherAccount } from './components';
 const store = useAuthStore()
 
 interface Props {
-	visible: boolean
+  visible: boolean
 }
 
 const backgroundImageURL = 'src/assets/login_bg.png'
-defineProps<Props>()
-const formRef = ref<HTMLElement & FormInst>();
+const formRef = ref<HTMLElement & FormInst>()
 
 interface LoginDTO {
-	userAccount: string
-	userPassword: string
+  userAccount: string
+  userPassword: string
 }
 
 const formRules = {
-	userAccount: {required: true, trigger: 'blur', message: '请输入用户名'},
-	userPassword: {required: true, trigger: 'blur', message: '请输入密码'}
+  userAccount: {
+    required: true,
+    trigger: 'blur',
+    message: '请输入用户名',
+  },
+  userPassword: {
+    required: true,
+    trigger: 'blur',
+    message: '请输入密码',
+  },
 }
 
 const formData = reactive<LoginDTO>({
-	userAccount: "terramours@163.com",
-	userPassword: "",
+  userAccount: 'terramours@163.com',
+  userPassword: '',
 })
 
 const loading = ref(false)
 const message = useMessage()
 const router = useRouter()
-const toRegister=async ()=>{
-	await router.push("/register")
+const toRegister = async () => {
+  await router.push('/register')
 }
 const loginHandle = () => {
-	formRef.value?.validate(async (err) => {
-		loading.value = true
-		if (err) {
-			loading.value = false
-			return
-		}
-		const loginRes = await login(formData)
-		loading.value = false
-		if (loginRes.code === 200) {
-			store.setToken(loginRes.data.token)
-			message.success('登录成功！')
-			await router.push("/chat")
-			return
-		}
-		message.warning('登录出错,请联系管理员')
-	})
-
+  formRef.value?.validate(async (err) => {
+    loading.value = true
+    if (err) {
+      loading.value = false
+      return
+    }
+    const loginRes = await login(formData)
+    loading.value = false
+    if (loginRes.code === 200) {
+      store.setToken(loginRes.data.token)
+      message.success('登录成功！')
+      await router.push('/chat')
+      return
+    }
+    message.warning('登录出错,请联系管理员')
+  })
 }
-
 </script>
+
+<template>
+  <div
+    class="h-full relative bg-center bg-cover bg-no-repeat"
+    :style="{ backgroundImage: `url(${backgroundImageURL})` }"
+  >
+    <NGrid cols="1 xs:10 m:6 l:24 xl:24 " item-responsive responsive="screen">
+      <NGridItem span="0 xs:2 m:2 l:14 xl:15" />
+      <NGridItem span="0 xs:6 m:2 l:6 xl:5">
+        <div class="translate-y-2/3">
+          <NSpin :show="loading">
+            <h2 class="text-white text-center pb-4 text-2xl font-mono font-bold">
+              TERRA MOURS
+            </h2>
+            <NForm ref="formRef" size="medium" label-placement="left" :model="formData" :rules="formRules">
+              <NFormItem path="userAccount">
+                <NInput v-model:value="formData.userAccount" placeholder="请输入用户名">
+                  <template #prefix>
+                    <SvgIcon icon="ant-design:user-outlined" />
+                  </template>
+                </NInput>
+              </NFormItem>
+              <NFormItem path="userPassword">
+                <NInput
+                  v-model:value="formData.userPassword" type="password" show-password-on="click"
+                  placeholder="请输入密码"
+                >
+                  <template #prefix>
+                    <SvgIcon icon="ant-design:unlock-outlined" />
+                  </template>
+                </NInput>
+              </NFormItem>
+              <NSpace :vertical="true" :size="24">
+                <div class="flex-y-center justify-between">
+                  <NSpace :size="15">
+                    <NCheckbox>
+                      <span class="text-slate-300">记住我</span>
+                    </NCheckbox>
+                    <NButton :text="true">
+                      <span class="text-slate-300">
+                        忘记密码？
+                      </span>
+                    </NButton>
+                  </NSpace>
+                </div>
+                <NButton
+                  type="primary"
+                  size="large"
+                  :block="true"
+                  @click="loginHandle"
+                >
+                  确定
+                </NButton>
+                <div class="flex-y-center justify-between">
+                  <!-- 手机验收码登录 -->
+                  <!-- <NButton class="flex-1" :block="true" @click="toLoginModule('code-login')">
+										{{ loginModuleLabels['code-login'] }}
+									</NButton> -->
+
+                  <div class="w-12px" />
+                  <NButton class="flex-1" :block="true" @click="toRegister">
+                    <span class="text-slate-300">
+                      注册
+                    </span>
+                  </NButton>
+                </div>
+              </NSpace>
+              <!-- 其他账户登录 -->
+              <!-- <other-account @login="handleLoginOtherAccount" /> -->
+            </NForm>
+          </NSpin>
+        </div>
+      </NGridItem>
+      <NGridItem span="0 xs:2 m:2 l:4 xl:4" />
+    </NGrid>
+    <!--    <div class="absolute w-1/4 p-4 right-40 bottom-1/2 translate-y-1/2"> -->
+    <!--   -->
+    <!--    </div> -->
+  </div>
+</template>
