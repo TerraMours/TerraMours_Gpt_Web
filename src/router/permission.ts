62a73b9373d675815ca9ba1c24a4c25c43f93b2f
yucc <e-Chuancheng.Yu@geely.com>
2023-07-11 16:37:11 +0800

    1.添加vue-virtual-scroll-list组件用于渲染chat页面大列表 2.添加dexie操作indexDB 3.回退路由鉴权 4.添加记住密码功能

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -4,27 +4,24 @@ import {useAuthStoreWithout} from '@/store/modules/auth'
 export const whiteList = ['/login']
 
 export function setupPageGuard(router: Router) {
-	console.log(router)
 
 	router.beforeEach(async (to, from, next) => {
 		const authStore = useAuthStoreWithout()
-		if (!authStore.session && to.path !== '/balance' && to.path !== '/image') {
-			try {
-				// const data = await authStore.getSession()
-				// if (String(data.auth) === 'false' && authStore.token)
-				//   authStore.removeToken()
-				if (to.path === '/500')
-					next({name: 'Root'})
-				else
-					next()
-			} catch (error) {
-				if (to.path !== '/500')
-					next({name: '500'})
-				else
-					next()
+		// 登录态
+		if (authStore.token) {
+			// 在登录时在地址栏输入login 将不跳转
+			if (whiteList.includes(to.path)) {
+				next({path: from.path!})
+			} else {
+				next()
 			}
 		} else {
-			next()
+			// 如果没有登录
+			if (whiteList.includes(to.path)) {
+				next()
+			} else {
+				next({name: 'login'})
+			}
 		}
 	})
 }
