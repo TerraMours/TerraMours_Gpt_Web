523b18c12a1e64f07e2f0d3973e03d2d6e849197
yucc <e-Chuancheng.Yu@geely.com>
2023-07-11 11:19:40 +0800

    修改登录逻辑:登录时router默认到chat页面 未登录时从地址栏输入chat地址无法进入需登录

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -1,28 +1,45 @@
-import type { Router } from 'vue-router'
-import { useAuthStoreWithout } from '@/store/modules/auth'
+import type {Router} from 'vue-router'
+import {useAuthStoreWithout} from '@/store/modules/auth'
+
+const whiteList = ['/login']
 
 export function setupPageGuard(router: Router) {
-  router.beforeEach(async (to, from, next) => {
-    const authStore = useAuthStoreWithout()
-    if (!authStore.session && to.path !== '/balance' && to.path !== '/image' ) {
-      try {
-        // const data = await authStore.getSession()
-        // if (String(data.auth) === 'false' && authStore.token)
-        //   authStore.removeToken()
-        if (to.path === '/500')
-          next({ name: 'Root' })
-        else
-          next()
-      }
-      catch (error) {
-        if (to.path !== '/500')
-          next({ name: '500' })
-        else
-          next()
-      }
-    }
-    else {
-      next()
-    }
-  })
+	router.beforeEach(async (to, from, next) => {
+		const authStore = useAuthStoreWithout()
+		// 登录态
+		if (authStore.token) {
+			// 在登录时在地址栏输入login 将不跳转
+			if (whiteList.includes(to.path)) {
+				next({path: from.path!})
+			} else {
+				next()
+			}
+		} else {
+			// 如果没有登录
+			if (whiteList.includes(to.path)) {
+				next()
+			} else {
+				next({name: 'login'})
+			}
+		}
+		/*--------------------------------*/
+		if (!authStore.session && to.path !== '/balance' && to.path !== '/image') {
+			try {
+				// const data = await authStore.getSession()
+				// if (String(data.auth) === 'false' && authStore.token)
+				//   authStore.removeToken()
+				if (to.path === '/500')
+					next({name: 'Root'})
+				else
+					next()
+			} catch (error) {
+				if (to.path !== '/500')
+					next({name: '500'})
+				else
+					next()
+			}
+		} else {
+			next()
+		}
+	})
 }
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -1,38 +1,40 @@
-import { ss } from '@/utils/storage'
+import {ss} from '@/utils/storage'
 
 const LOCAL_NAME = 'userStorage'
 
 export interface UserInfo {
-  avatar: string
-  name: string
-  description: string
-  userName?: string// 用户名
-  roleId?: number// 角色
-  headImageUrl?: string//头像url
-  vipLevel?: string//vip等级
-  vipExpireTime?: string//vip过期时间
-  imageCount?: string//剩余图片使用次数
+	avatar: string
+	name: string
+	description: string
+	userName?: string// 用户名
+	roleId?: number// 角色
+	headImageUrl?: string//头像url
+	vipLevel?: string//vip等级
+	vipExpireTime?: string//vip过期时间
+	imageCount?: string//剩余图片使用次数
+	userAccount?: string//账号（用于记住密码选项）
+	userPassword?: string // 密码 （用于记住密码选项）
 }
 
 export interface UserState {
-  userInfo: UserInfo
+	userInfo: UserInfo
 }
 
 export function defaultSetting(): UserState {
-  return {
-    userInfo: {
-      avatar: 'https://avatars.githubusercontent.com/u/43564692?s=96&v=4',
-      name: 'firstsaofan',
-      description: '最新gpt地址： <a href="https://ai.terramours.site/" class="text-blue-500" target="_blank" >ai.terramours.site</a>',
-    },
-  }
+	return {
+		userInfo: {
+			avatar: 'https://avatars.githubusercontent.com/u/43564692?s=96&v=4',
+			name: 'firstsaofan',
+			description: '最新gpt地址： <a href="https://ai.terramours.site/" class="text-blue-500" target="_blank" >ai.terramours.site</a>',
+		},
+	}
 }
 
 export function getLocalState(): UserState {
-  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
-  return { ...defaultSetting(), ...localSetting }
+	const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+	return {...defaultSetting(), ...localSetting}
 }
 
 export function setLocalState(setting: UserState): void {
-  ss.set(LOCAL_NAME, setting)
+	ss.set(LOCAL_NAME, setting)
 }
