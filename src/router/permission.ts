01881fed23c2f75e78f54af577d995620ebfb3c8
raokun <272728816@qq.com>
2023-09-08 10:54:21 +0800

    优化：未登录用户有预览权限，注释路由判断方法。通过接口的返回来判断
    优化：未登录用户有预览权限，注释路由判断方法。通过接口的返回来判断

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -1,33 +1,31 @@
-import type {Router} from 'vue-router'
-import {useAuthStoreWithout} from '@/store/modules/auth'
+import type { Router } from 'vue-router'
+import { useAuthStoreWithout } from '@/store/modules/auth'
 
 export const whiteList = ['/login']
 
 export function setupPageGuard(router: Router) {
-
-	router.beforeEach(async (to, from, next) => {
-		const authStore = useAuthStoreWithout()
-		//注册和查询余额页面不需要登录验证
-		if (!authStore.session && to.path !== '/balance' && to.path !== '/register') {
-		// 登录态
-		if (authStore.token) {
-			// 在登录时在地址栏输入login 将不跳转
-			if (whiteList.includes(to.path)) {
-				next({path: from.path!})
-			} else {
-				next()
-			}
-		} else {
-			// 如果没有登录
-			if (whiteList.includes(to.path)) {
-				next()
-			} else {
-				next({name: 'login'})
-			}
-		}
-	}
+  router.beforeEach(async (to, from, next) => {
+    const authStore = useAuthStoreWithout()
+    // 注册和查询余额页面不需要登录验证
+    if (!authStore.session && to.path !== '/balance' && to.path !== '/register') {
+      // 登录态
+      if (authStore.token) {
+        // 在登录时在地址栏输入login 将不跳转
+        if (whiteList.includes(to.path))
+          next({ path: from.path! })
+			 else
+          next()
+      }
+      else {
+        // 如果没有登录
+        if (whiteList.includes(to.path))
+          next()
+			 else
+          next({ name: 'login' })
+      }
+    }
     else {
       next()
     }
-	})
+  })
 }
